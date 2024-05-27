e556cb749fc4b09af04373c36ebdb302ebd50164
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-05-27 15:05:09 +1000

    clear 19:12

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
+    case ACTIONS.CLEAR:
+      return {}
   }
 }
 
@@ -37,7 +39,7 @@ function App() {
         <div className="previous-operand">{previousOperand} {operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
       <button>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
